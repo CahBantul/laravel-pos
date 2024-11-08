e30683979bfeff6c0951ec30279a7da0c9b569d3
Programmer Telo <programertelo@gmail.com>
2024-11-08 21:42:04 +0700

    feat: adding model, migration and seeder

---
+++ database/seeders/DistrictSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class DistrictSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Path ke file SQL
+        $sqlPath = database_path('seeders/data/laravel_pos_districts.sql');
+
+        // Cek apakah file SQL ada
+        if (File::exists($sqlPath)) {
+            // Baca isi file SQL
+            $sql = File::get($sqlPath);
+
+            // Jalankan isi file SQL
+            DB::unprepared($sql);
+
+            $this->command->info('Regencies data seeded successfully!');
+        } else {
+            $this->command->error('File SQL tidak ditemukan.');
+        }
+    }
+}
