f809eedd85000be5be15bffb89fcc54d1b5c7daf
Programmer Telo <programertelo@gmail.com>
2024-11-28 05:50:17 +0700

    fix: models

--- app/Models/District.php
+++ app/Models/District.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class District extends Model
 {
-    //
+    public function regency() : BelongsTo
+    {
+        return $this->belongsTo(Regency::class);
+    }
 }
