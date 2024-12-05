665e4e9c257c37b8dc7f73f0c138653178ffa66a
Programmer Telo <programertelo@gmail.com>
2024-12-05 13:35:34 +0700

    feat: adding role permission

--- app/Filament/Resources/SupplierResource.php
+++ app/Filament/Resources/SupplierResource.php
@@ -4,15 +4,9 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\SupplierResource\Pages;
 use App\Filament\Resources\SupplierResource\RelationManagers;
-use App\Models\District;
-use App\Models\Regency;
 use App\Models\Supplier;
-use App\Models\Village;
 use Filament\Forms;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
-use Filament\Forms\Get;
-use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -29,71 +23,59 @@ class SupplierResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Section::make('Company Information')
-                ->schema([
-                    Forms\Components\TextInput::make('name')
-                        ->required()
-                        ->maxLength(100),
-                    Forms\Components\TextInput::make('email')
-                        ->email()
-                        ->required()
-                        ->maxLength(100),
-                    Forms\Components\TextInput::make('phone')
-                        ->tel()
-                        ->required()
-                        ->maxLength(20),
-                ])
-                ->columns(3),
-            Section::make('Company Address')
-                ->schema([
-                    Forms\Components\TextInput::make('address')
-                        ->maxLength(200)
-                        ->columnSpanFull(),
-                    Forms\Components\Select::make('province_id')
-                        ->relationship('province', 'name')
-                        ->searchable()
-                        ->preload()
-                        ->live()
-                        ->afterStateUpdated(function (Set $set) {
-                            $set('regency_id', null);
-                            $set('district_id', null);
-                            $set('village_id', null);
-                        })
-                        ->required(),
-                    Forms\Components\Select::make('regency_id')
-                        ->options(fn(Get $get) => Regency::query()->where('province_id', $get('province_id'))->pluck('name', 'id'))
-                        ->searchable()
-                        ->preload()
-                        ->live()
-                        ->afterStateUpdated(function (Set $set) {
-                            $set('district_id', null);
-                            $set('village_id', null);
-                        })
-                        ->required(),
-                    Forms\Components\Select::make('district_id')
-                        ->options(fn(Get $get) => District::query()->where('regency_id', $get('regency_id'))->pluck('name', 'id'))
-                        ->searchable()
-                        ->preload()
-                        ->live()
-                        ->afterStateUpdated(function (Set $set) {
-                            $set('village_id', null);
-                        })
-                        ->required(),
-                    Forms\Components\Select::make('village_id')
-                        ->options(fn(Get $get) => Village::query()->where('district_id', $get('district_id'))->pluck('name', 'id'))
-                        ->searchable()
-                        ->preload()
-                        ->required(),
-                ])
-                ->columns(2)
-        ]);
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(20),
+                Forms\Components\TextInput::make('address')
+                    ->maxLength(200),
+                Forms\Components\Select::make('province_id')
+                    ->relationship('province', 'name')
+                    ->required(),
+                Forms\Components\Select::make('regency_id')
+                    ->relationship('regency', 'name')
+                    ->required(),
+                Forms\Components\Select::make('district_id')
+                    ->relationship('district', 'name')
+                    ->required(),
+                Forms\Components\Select::make('village_id')
+                    ->relationship('village', 'name')
+                    ->required(),
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('province.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('regency.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('district.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('village.name')
+                    ->numeric()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
